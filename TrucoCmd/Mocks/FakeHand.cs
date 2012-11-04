644f4bb5a9137b3c2a60ece80c6250b66057426c
Fabio Pagoti <fabiopagoti@gmail.com>
2012-11-04 04:37:07 -0800

    Fake Hand added

---
+++ TrucoCmd/Mocks/FakeHand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CardGameFramework.Model.Card;
+using CardGameFramework.Model.Deck;
+
+namespace CardGameFramework.Mocks
+{
+    public class FakeHand
+    {
+
+        public static Hand createHand(Suit suit)
+        {
+            Hand hand = new Hand();
+            hand.Cards.AddRange(
+                new List<Card>()
+                    {
+                        new Card(suit, FaceValue.Ace),
+                        new Card(suit, FaceValue.Two),
+                        new Card(suit, FaceValue.Three)
+                    });
+            return hand;
+        }
+
+    }
+}
