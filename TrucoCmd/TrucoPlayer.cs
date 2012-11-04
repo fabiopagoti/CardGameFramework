66ff0298854ee18c3a0c196c0bd0d20c44a3ae5b
Fabio Pagoti <fabiopagoti@gmail.com>
2012-11-04 05:11:52 -0800

    Solve Truco Player conflict

--- TrucoCmd/TrucoPlayer.cs
+++ TrucoCmd/TrucoPlayer.cs
@@ -9,21 +9,12 @@ namespace TrucoCmd
         public string Name { get; set; }
         public Hand Hand { get; set; }
         public ICollection<TrucoPlayer> Partners { get; set; }
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  
-=======
-=======
->>>>>>> TrucoMocks
+
 
         public TrucoPlayer(string name)
         {
             Name = name;
         }
-<<<<<<< HEAD
-        
->>>>>>> Stashed changes
-=======
->>>>>>> TrucoMocks
+
     }
 }
