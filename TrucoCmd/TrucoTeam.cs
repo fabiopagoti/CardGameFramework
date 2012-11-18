dd6dd9af03a4f9f53ff7cb1acb50dcf5d8205652
Fabio Pagoti <fabiopagoti@gmail.com>
2012-11-18 07:33:28 -0800

    Truco team tests

--- TrucoCmd/TrucoTeam.cs
+++ TrucoCmd/TrucoTeam.cs
@@ -8,10 +8,18 @@ namespace TrucoCmd
         public string Name { get; set; }
         public ICollection<TrucoPlayer> Players;
 
-
         public TrucoTeam(ICollection<TrucoPlayer> players)
         {
             this.Players = players;
         }
+
+        public bool isPlayerFromTeam(TrucoPlayer trucoPlayer)
+        {
+            return false ;
+
+        }
+
+
+
     }
 }
