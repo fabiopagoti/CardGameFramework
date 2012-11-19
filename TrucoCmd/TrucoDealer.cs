cdc55ec363b4f832d6fd5bd899ec0ef63e57a8c7
Fabio Pagoti <fabiopagoti@gmail.com>
2012-11-19 08:20:15 -0800

    Creation of Truco Dealer and Unit test class

---
+++ TrucoCmd/TrucoDealer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CardGameFramework.Model.Deck;
+using CardGameFramework.Model.Game;
+using CardGameFramework.Model.Player;
+
+namespace TrucoCmd
+{
+    public class TrucoDealer : IDealer
+    {
+        #region Implementation of IDealer
+
+        public void DistributeCards(ICardGame cardGame, IDeck deck, ICollection<IPlayer> players)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DistributeCardsToSinglePlayer(ICardGame cardGame, IDeck deck, IPlayer player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+        #endregion
+    }
+}
